fe3913b4c5b0c0239a5eb4e0b9a59a648fbb224e
Gabriel Zanin <gabrielfernandocazequezanin@protonmail.ch>
2021-09-29 09:18:29 -0300

    Controller e Model OS

---
+++ PetShop/Models/OS.cs
@@ -0,0 +1,22 @@
+namespace PetShop.Models
+{
+    public class OS
+    {
+        public int IdOs { get; set; }
+
+        public string NomeCliente { get; set; }
+
+        public string NomeServico { get; set; }
+
+          public string NomePet {get; set;}
+
+          public int QuantidadeServico { get; set; }
+
+        public double ValorServico {get; set;}
+    
+        public string TipoPetOS {get; set;}
+
+        public double ValorTotalOs { get; set; }
+
+    }
+}
