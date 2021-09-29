16d9d098a8c226f5f478816f6c61b8ca4eece13f
Gabriel Zanin <gabrielfernandocazequezanin@protonmail.ch>
2021-09-28 23:25:42 -0300

    Pessoa Controller
    Inicio cadastro pessoa

---
+++ PetShop/Controllers/PessoaController.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace PetShop.Controllers
+{
+    [ApiController]
+    [Route("petshop/pessoa")]
+    public class PessoaController : ControllerBase
+    {
+        
+        public void Create()
+        {
+            
+        }
+
+
+
+
+    }
+}
