387f5a9e8d319e403abcca0a90992f87c479e65c
cneres <58155380+cneres@users.noreply.github.com>
2021-09-30 09:55:42 -0300

    services

---
+++ PetShop/FRONT/src/services/pet.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Pet } from 'src/models/pet';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PetService {
+
+  constructor(private http: HttpClient )
+   {
+
+   }
+
+   list(): Pet[]{
+
+    return[]
+   }
+}
